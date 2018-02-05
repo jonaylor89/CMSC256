a4840f003457a6ec06c11b3154bad082b94aab48
John Naylor <jonaylor89@gmail.com>
2018-02-05 14:34:41 -0500

    Initial Commit

---
+++ projects/Project2/PersonalManager.java
@@ -0,0 +1,7 @@
+
+public class PersonalManager {
+    
+    public PersonalManager() {
+    
+    }
+}
---
+++ projects/Project2/Project2.java
@@ -0,0 +1,13 @@
+
+public class Project2 {
+    public static void main(String[] argv) {
+        printHeading();
+    }
+
+    private static void printHeading() {
+        System.out.println("John Naylor");
+        System.out.println("2/5/18");
+        System.out.println("CMSC256 Section2 Spring");
+        System.out.println("Project 2: Program to manage employees like a true capitalist pig");
+    }
+}
