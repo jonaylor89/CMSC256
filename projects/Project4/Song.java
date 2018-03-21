5ba386894840feffbebcad668fb060416d84ecd2
jonaylor89 <jonaylor89@gmail.com>
2018-03-21 14:57:29 -0400

    boilerplate code

--- projects/Project4/Song.java
+++ projects/Project4/Song.java
@@ -1,6 +1,9 @@
-/**
+/*********************************
+ * John Naylor
+ * CMSC 256 Section 2 
+ * Project 4 Song project
  * 
- */
+ *******************************/
 
 public class Song implements Comparable{
 
@@ -8,22 +11,83 @@ public class Song implements Comparable{
     private String title;
     private String artist;
 
+    /***************************************
+     * @param album The song's album
+     * @param title The song's title
+     * @param artist The song's artist
+     **************************************/
     public Song(String album, String title, String artist) {
-        
+        setAlbum(album);
+        setTitle(title);
+        setArtist(artist);
     }
 
+    /**************************************
+     * @param album the album to set
+     ***********************************/
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    /*************************************
+     * @param artist the artist to set
+     ************************************/
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    /***********************************
+     * @param title the title to set
+     **********************************/
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**************************
+     * @return the album
+     **************************/
+    public String getAlbum() {
+        return album;
+    }
+
+    /****************************
+     * @return the artist
+     ***************************/
+    public String getArtist() {
+        return artist;
+    }
+
+    /******************************
+     * @return the title
+     *****************************/
+    public String getTitle() {
+        return title;
+    }
+
+    /***************************************
+     * @return If two songs are easier
+     **************************************/
     @Override
     public boolean equals(Object obj) {
-        return true;
+        return true;                                              // TODO 
     }
 
+    /***********************************************
+     * @return String representation of a song
+     ***********************************************/
     @Override
     public String toString() {
-        return "";
+        return "";                                                // TODO
     }
 
+    /**************************************
+     * @return Comparison of two songs
+     *************************************/
     @Override
     public int compareTo(Object obj) {
-        return 0;
+        return 0;                                                // TODO
     }
+
+
+
 }
--- projects/Project4/SongReader.java
+++ projects/Project4/SongReader.java
@@ -1,6 +1,9 @@
-/**
+/*********************************
+ * John Naylor
+ * CMSC 256 Section 2 
+ * Project 4 Song Reader
  * 
- */
+ *******************************/
 
 public class SongReader {
     public static void main(String[] args) {
